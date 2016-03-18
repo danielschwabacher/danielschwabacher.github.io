0b6e9bffd5b5d344366f93203df355bb347a0acf
Daniel Schwabacher <danielschwabacher@gmail.com>
2016-03-18 15:38:10 -0600

    Initial commit

---
+++ assets/php/handleFormSubmit.php
@@ -0,0 +1,19 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = $visitor_email;
+  $email_subject = "New Form submission";
+  $email_body = "You have received a new message from the user $name.\n".
+      "Here is the message:\n $message".
+
+
+  $to = "danielschwabacher@gmail.com";
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+
+?>
